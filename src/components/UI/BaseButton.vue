f7d18bfe8a9cba1b0b3b1d4d0dadc16e15d1c284
TeutaUkshini <44062876+TeutaUkshini@users.noreply.github.com>
2021-11-22 17:56:13 +0100

    Add files via upload

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode']
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #9b691f79;
+  border: 1px solid #9b691f79;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #68625e;
+  border-color: #68625e;
+}
+
+.flat {
+  background-color: transparent;
+  color: #000000;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #948b6d;
+}
+</style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #9b691f99;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  color: black;
+  margin: 0;
+}
+</style>
